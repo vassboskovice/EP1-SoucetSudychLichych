c238bbe6e616f0cddc5c15dc9605309476b5e0d5
Jan Vintr <Vintr.Jan@stud.vassboskovice.cz>
2024-06-04 09:00:57 +0200

    5. počet zadávaných čísel

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,21 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main()
+
+        Console.Write("Zadejte celkový poèet èísel")
+        Dim totalNumbers As Integer = Integer.Parse(Console.ReadLine())
+
+        Dim numbers(totalNumbers - 1) As Integer
+
+        For i As Integer = 0 To totalNumbers - 1
+            Console.Write("Zadejte èíslo" & (i + 1) & ": ")
+        Next
+        Console.WriteLine("Zadaná èísla jsou: ")
+        For Each num As Integer In numbers
+            Console.WriteLine(num)
+        Next
+        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení.")
+        Console.ReadKey()
+    End Sub
 End Module
